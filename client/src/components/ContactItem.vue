1c380293559ed17c5a0e9284b6384f586bc427cd
Drew <an538926@dal.ca>
2022-04-09 12:25:19 -0300

    first commit

---
+++ client/src/components/ContactItem.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <!-- This should be a login page that the user sees as soon as they reach the page -->
+  <div class="header">
+    <h1 class="header-text">SP Dashboard 2.0</h1>
+  </div>
+</template>
---
+++ client/src/views/ContactView.vue
@@ -0,0 +1,9 @@
+<script setup>
+// this is where imports can go
+</script>
+
+<template>
+  <main>
+    <h1>Contact</h1>
+  </main>
+</template>
